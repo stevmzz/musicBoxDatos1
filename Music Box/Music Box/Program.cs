df29ddd324f67f87e681f2ba3fd5d04a4cb2fb1e
stevmzz <stevenaguilaralvarezz@gmail.com>
2025-01-10 23:10:46 -0600

    [ADD] Proyecto base

---
+++ Music Box/Music Box/Program.cs
@@ -0,0 +1,48 @@
+﻿using NAudio.Wave;
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        // Frecuencias de las notas (en Hz)
+        var notas = new Dictionary<string, double>
+        {
+            {"Do", 261.63},
+            {"Re", 293.66},
+            {"Mi", 329.63},
+            {"Fa", 349.23},
+            {"Sol", 392.00},
+            {"La", 440.00},
+            {"Si", 493.88}
+        };
+
+        Console.WriteLine("Reproduciendo escala musical...");
+        
+        foreach (var nota in notas)
+        {
+            Console.WriteLine($"Reproduciendo {nota.Key} ({nota.Value} Hz)");
+            
+            var sineWaveProvider = new SignalGenerator()
+            {
+                Gain = 0.2,
+                Frequency = nota.Value,
+                Type = SignalGeneratorType.Sin
+            };
+
+            using (var waveOut = new WaveOut())
+            {
+                waveOut.Init(sineWaveProvider);
+                waveOut.Play();
+                Thread.Sleep(500); // Duración de cada nota (500ms)
+                waveOut.Stop();
+            }
+
+            Thread.Sleep(100); // Pequeña pausa entre notas
+        }
+
+        Console.WriteLine("¡Escala completada!");
+        Console.ReadLine();
+    }
+}
