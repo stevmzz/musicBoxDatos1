7dc03f385514197cc6131b9c1a99b48478aababf
stevmzz <stevenaguilaralvarezz@gmail.com>
2025-01-12 18:30:58 -0600

    [ADD] Clase base de DoublyLinkedList con sus diferentes métodos

--- Music Box/Music Box/Models/DoublyLinkedList.cs
+++ Music Box/Music Box/Models/DoublyLinkedList.cs
@@ -1,19 +1,59 @@
 namespace Music_Box.Models {
     public class DoublyLinkedList {
+
         private Node? Head;
         private Node? Tail;
         private int Count;
+        private Node? currentNote;
 
         public DoublyLinkedList() {
             Head = null;
             Tail = null;
             Count = 0;
         }
-        // Aquí irán los métodos:
-        // addNote
-        // removeNote
-        // getNext
-        // getPrevious
-        // etc...
+
+        public void addNote(Note note) {
+            Node newNode = new Node(note);
+            
+            if (Count == 0) { 
+                Head = newNode;
+                Tail = newNode;
+                currentNote = newNode;
+            }
+
+            else {
+                newNode.Previus = Tail;
+                Tail!.Next = newNode;
+                Tail = newNode;
+            }
+            Count += 1;
+        }
+
+        public Node? GetNext() {
+            if (currentNote != null) {
+                return currentNote.Next;
+            }
+            return null;
+        }
+
+        public Node? GetPrevious(Node currentNode) {
+            return currentNode.Previus;
+        }
+
+        public Node? GetFirst() {
+            return Head;
+        }
+
+        public Node? GetLast() {
+            return Tail;
+        }
+
+        public int GetCount() {
+            return Count;
+        }
+
+        public bool IsEmpty() {
+            return Count == 0;
+        }
     }
 }
