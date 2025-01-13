5537f5ba2e0fb0772f6855808b2305530cf19b7b
stevmzz <stevenaguilaralvarezz@gmail.com>
2025-01-12 22:48:37 -0600

    [ADD] Método get y set para el nodo actual y código comentado

--- Music Box/Music Box/Models/DoublyLinkedList.cs
+++ Music Box/Music Box/Models/DoublyLinkedList.cs
@@ -1,50 +1,67 @@
+using System.Timers;
+
 namespace Music_Box.Models {
-    public class DoublyLinkedList {
+    public class DoublyLinkedList { // clase para la lista doblemente enlazada
 
-        private Node? Head;
-        private Node? Tail;
-        private int Count;
-        private Node? currentNote;
+        private Node? Head; // nodo inical (cabeza)
+        private Node? Tail; // nodo final (cola)
+        private int Count; // contador de nodos
+        private Node? currentNote; // nodo actual seleccionado
 
-        public DoublyLinkedList() {
+        public DoublyLinkedList() { // constructor
             Head = null;
             Tail = null;
             Count = 0;
         }
 
-        public void addNote(Note note) {
-            Node newNode = new Node(note);
+        public void addNote(Note note) { // método para agregar un nodo a la lista
+            Node newNode = new Node(note); // crea un nodo con la nota
             
-            if (Count == 0) { 
-                Head = newNode;
+            if (Count == 0) { // si la lista es vacía el único nodo será "todos"
+                Head = newNode; 
                 Tail = newNode;
                 currentNote = newNode;
             }
 
             else {
-                newNode.Previus = Tail;
-                Tail!.Next = newNode;
-                Tail = newNode;
+                newNode.Previus = Tail; // el nodo anterior apunta a la cola
+                Tail!.Next = newNode; // el siguiente nodo apunta al nuevo nodo
+                Tail = newNode; // el nuevo nodo es la cola
             }
             Count += 1;
         }
 
-        public Node? GetNext() {
+        public Node? GetNext() { // método para obtener el siguiente nodo desde el actual
             if (currentNote != null) {
                 return currentNote.Next;
             }
             return null;
         }
 
-        public Node? GetPrevious(Node currentNode) {
-            return currentNode.Previus;
+        public Node? GetPrevious() { // método para obtener el anterior nodo desde el actual
+            if (currentNote?.Previus != null) {
+                currentNote = currentNote.Previus;
+                return currentNote;
+            }
+            return null;
+        }
+
+        // getters and setters básicos
+        public Node? GetCurrentNote() {
+            return currentNote;
+        }
+
+        public void SetCurrentNote(Node? node) {
+            currentNote = node;
         }
 
         public Node? GetFirst() {
+            currentNote = Head;
             return Head;
         }
 
         public Node? GetLast() {
+            currentNote = Tail;
             return Tail;
         }
 
@@ -52,7 +69,7 @@ namespace Music_Box.Models {
             return Count;
         }
 
-        public bool IsEmpty() {
+        public bool IsEmpty() { // método para ver si la lista es vacía
             return Count == 0;
         }
     }
