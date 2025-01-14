a21e305994e4e7f43dff45d5e0f63e875b83889c
stevmzz <stevenaguilaralvarezz@gmail.com>
2025-01-13 18:34:50 -0600

    [ADD] Reproducción de notas y establecer velocidad

--- Music Box/Music Box/Models/Note.cs
+++ Music Box/Music Box/Models/Note.cs
@@ -1,17 +1,28 @@
 namespace Music_Box.Models {
     public class Note {
-        public string NoteName {get; set;}
-        public double Frequency {get; set;}
-        public string Duration {get; set;}
+        public string NoteName {get; set;} // nombre de la nota además de get y set
+        public double Frequency {get; set; } // frecuencia de la nota además de get y set
+        public string Duration {get; set; } // duración de la nota además de get y set
 
         public Note(string noteName, string duration) {
             NoteName = noteName;
             Duration = duration;
-            Frequency = getFrequency(noteName); // por implementar
+            Frequency = getFrequency(noteName);
         }
 
-        private double getFrequency(string note) {
-            return 440.0; // por ahora valor por defecto
+        private double getFrequency(string note) // método para obtener las frecuencias respectivas
+        {
+            return note switch
+            {
+                "Do" => 261.63,
+                "Re" => 293.66,
+                "Mi" => 329.63,
+                "Fa" => 349.23,
+                "Sol" => 392.00,
+                "La" => 440.00,
+                "Si" => 493.88,
+                _ => 440.00
+            };
         }
     }
 }
